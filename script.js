dd7220132433d18798d009810197b5e971d34c9a
sakshi-mishra732 <sakshimishra8723@gmail.com>
2025-09-08 00:54:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let inputBox=document.querySelector("#inputbox");
+
+let buttons=document.querySelectorAll("button");
+
+let string = "";
+buttons.forEach(button =>{
+    button.addEventListener("click",(evt)=>{
+         if(evt.target.innerText =="="){
+string = eval(string);
+inputBox.value = string;
+         }
+         else if(evt.target.innerText=="AC"){
+            string = ""
+            inputBox.value = string;
+         }
+         else if(evt.target.innerText =="DEL"){
+            string=string.toString();
+            string=string.substring(0,string.length-1)
+            inputBox.value = string;
+         }
+         else{
+            string = string +evt.target.innerText;
+            inputBox.value = string;
+         }
+        
+    })
+    
+})
